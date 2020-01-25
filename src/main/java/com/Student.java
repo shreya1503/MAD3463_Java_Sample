1a9f760b24b15eabf953dcba3a2858ba380be298
SHREYA <shreyavaghasia99@gmail.com>
2020-01-24 20:41:59 -0500

    Student Details

--- src/main/java/com/Student.java
+++ src/main/java/com/Student.java
@@ -1,5 +1,46 @@
 package com;
 
+import java.util.Date;
+
+enum Gender
+{
+    MALE,
+    FEMALE,
+    OTHERS
+}
+
 public class Student
 {
+
+    int studentId;
+    String firstName;
+    String lastName;
+    Date birthDate;
+    Gender gender;
+    float totalMarks;
+
+    public void setData(int studentId, String firstName, String lastName, Date birthDate, Gender gender, float totalMarks )
+    {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.totalMarks = totalMarks;
+
+    }
+
+    public void printData()
+    {
+        System.out.println("Student id : " + studentId);
+        System.out.println("First Name : " + firstName);
+        System.out.println("Last Name  : " + lastName);
+        System.out.println("Birth Date : " + birthDate);
+        System.out.println("Gender : " + gender);
+        System.out.println("Total Marks : " + totalMarks);
+
+    }
+
+    
+
 }
--- src/main/java/com/StudentClassMain.java
+++ src/main/java/com/StudentClassMain.java
@@ -1,9 +1,14 @@
 package com;
 
-public class StudentClassMain
-{
-    public static void main(String[] args)
-    {
+import java.util.Date;
 
+public class StudentClassMain {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.setData(1, "Shreya", "Vaghasiya", new Date(2020, 0, 24),
+                Gender.FEMALE, 70);
+
+        s1.printData();
     }
 }
